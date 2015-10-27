f071df1074da838bed744bdc159f579a1c8a5e93
Anders Gärdenäs <anders.gardenas@gmail.com>
2015-10-27 19:55:45 +0100

    upddated exe5

--- LACPP-A3/src/exercise5.cpp
+++ LACPP-A3/src/exercise5.cpp
@@ -5,21 +5,30 @@ using namespace std;
 
 int main () {
   bool run1 = false;
-  bool run2 = false;
-  bool run3 = false;
     __transaction_atomic
       {
 	run1 = true;
 	  __transaction_atomic
 	    {
-	      run2 = true;
 	      __transaction_cancel; 
 	    }
-	  run3 = true;
       }
-    cout << "the outer trasaction is commited " <<  run1 << endl;
-    cout << "the inner trasaction is commited " <<  run2 << endl;
-    cout << "the outer transaction is committed after the inner transaction has raised an exception " <<  run3 << endl;
-    
+        cout << "the outer trasaction is not aborted doue to iner trasaction aborting " <<  run1 << endl;
+
+
+	  
+  bool run2 = false;
+  bool run3 = false;
+  run2 = true;
+  __transaction_atomic
+    {
+      if (run2){
+	run3 = true;
+      }
+    }
+  
+  
+  cout << "the inner trasaction can se changes done by the outer transaction " <<  run3 << endl;
+  
   
 }
